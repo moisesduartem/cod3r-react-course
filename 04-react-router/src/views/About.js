e8b0b1475252a9fa6421779d961168c57afd95db
Moisés Mariano <moises@softbuilder.com.br>
2021-01-20 12:40:29 -0300

    feat: creating first components

---
+++ 04-react-router/src/views/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+            <h1>About</h1>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores similique numquam quidem praesentium doloremque maxime architecto molestias, nisi omnis pariatur quis iusto officiis voluptatibus! Reiciendis quos quisquam rerum debitis sunt!</div>
+        </>
+
+    )
+}
+
+export default About
