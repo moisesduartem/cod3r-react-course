d912102fc0610538f92839d4cf5ff365602a78da
Moisés Mariano <moises@softbuilder.com.br>
2021-01-21 08:06:22 -0300

    useState example

--- 05-react-hooks/src/views/examples/UseState.jsx
+++ 05-react-hooks/src/views/examples/UseState.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseState = (props) => {
+
+    const initialValue = 0;
+
+    const [myState, setMyState] = useState(initialValue)
+
     return (
         <div className="UseState">
+            <h1>{myState}</h1>
+
+            <button className="btn" onClick={(e) => setMyState(myState + 1)}>+</button>
+            <button className="btn" onClick={(e) => setMyState(myState - 1)}>-</button>
+            <button className="btn" onClick={(e) => setMyState(initialValue)}>C</button>
+
+            <hr />
+
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
