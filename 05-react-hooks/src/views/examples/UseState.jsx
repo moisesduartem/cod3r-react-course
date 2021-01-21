e1e7aedbc70871d5f15ee1e855d6d1fcf9239565
Moisés Mariano <moises@softbuilder.com.br>
2021-01-21 08:14:42 -0300

    using useState to manage a input value onChange

--- 05-react-hooks/src/views/examples/UseState.jsx
+++ 05-react-hooks/src/views/examples/UseState.jsx
@@ -1,26 +1,47 @@
 import React, { useState } from 'react';
 import PageTitle from '../../components/layout/PageTitle'
 
+const classes = {
+    button: {
+        width: 50,
+        height: 50,
+        fontSize: 14
+    }
+};
+
 const UseState = (props) => {
 
     const initialValue = 0;
 
     const [myState, setMyState] = useState(initialValue)
+    const [myText, setMyText] = useState('');
 
     return (
         <div className="UseState">
-            <h1>{myState}</h1>
-
-            <button className="btn" onClick={(e) => setMyState(myState + 1)}>+</button>
-            <button className="btn" onClick={(e) => setMyState(myState - 1)}>-</button>
-            <button className="btn" onClick={(e) => setMyState(initialValue)}>C</button>
-
-            <hr />
 
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+
+            <h1>Exercício #01</h1>
+
+            <hr />
+
+            <h1>{myState}</h1>
+
+            <div>
+                <button style={classes.button} onClick={(e) => setMyState(myState + 1)}>+</button>
+                <button style={classes.button} onClick={(e) => setMyState(myState - 1)}>-</button>
+                <button style={classes.button} onClick={(e) => setMyState(initialValue)}>C</button>
+            </div>
+            <h1>Exercício #02</h1>
+
+            <hr />
+
+            <input onChange={(e) => { setMyText(e.target.value) }} placeholder="Digite alguma coisa" />
+            <span style={{ color: 'red' }}>{myText}</span>
+
         </div>
     )
 }
