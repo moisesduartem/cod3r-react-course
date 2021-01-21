91ebe3518b1aee7d91b5a01b78f203bf136d36fb
Moisés Mariano <moisesduartem@gmail.com>
2021-01-20 21:34:46 -0300

    feat: 404 error with react router

--- 04-react-router/src/components/layout/Content.js
+++ 04-react-router/src/components/layout/Content.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import About from '../../views/About';
 import Home from '../../views/Home';
+import NotFound from '../../views/NotFound';
 import User from '../../views/User';
 import UserList from '../../views/UserList';
 
@@ -14,6 +15,7 @@ function Content() {
                 <Route exact path="/about" component={About} />
                 <Route exact path="/users" component={UserList} />
                 <Route exact path="/users/:id" component={User} />
+                <Route path="*" component={NotFound} />
             </Switch>
         </BrowserRouter>
     )
